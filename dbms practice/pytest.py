83133503a7d932ca06ae510f0cfd85721cad529d
VijayDaloiya <63780543+VijayDaloiya@users.noreply.github.com>
2021-05-07 13:20:31 +0530

    Update pytest.py

--- dbms practice/pytest.py
+++ dbms practice/pytest.py
@@ -1,7 +1,18 @@
 import pandas as pd
 from sqlalchemy import create_engine
+cols = [2,3]
+df = pd.read_excel('EmployeeList.xlsx', engine='openpyxl',usecols=cols)
 
-df = pd.read_excel('EmployeeList.xlsx', engine='openpyxl')
+engine = create_engine('mysql+mysqlconnector://root:oreo0008@localhost/practice'.format(user='root', password='oreo0008', server='localhost', database='practic'))
 
-engine = create_engine('mysql+mysqlconnector://root:oreo0008@localhost/practice'.format(user='root', password='oreo0008', server='localhost', database='practice'))
-df.to_sql('people', con=engine)
+df.to_sql('people', con=engine) 
+# /* to make any queries in database form python file */
+# query=''' .....''''
+# df=pd.read_sql_query(query,engine)
+
+#/* name of sql coloumn n csv file coloumn should be same , can be achieved in python file by */
+ #df.rename(columns={
+ #  'oldname': 'newname'
+ # }inplace=True)
+
+ 
